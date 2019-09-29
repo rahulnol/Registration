8d7a6f4503eef8fa0a2bd7991b288657effb7af5
rahulnol <rahulrdhp@gmail.com>
2019-09-29 13:16:29 +0530

    Implement account verification link feature

--- registration/urls.py
+++ registration/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from django.conf.urls import re_path
+from django.views.generic import TemplateView
+
 from registration import views
 
 urlpatterns = [
     path('login/',views.signin),
     path('signup/',views.signUp),
+    path('account/activate/', TemplateView.as_view(template_name='registration/account_activation.html')),
+    re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.activate, name='activate'),
 ]
--- registration/views.py
+++ registration/views.py
@@ -1,8 +1,19 @@
+import json
+
 from django.contrib.auth import authenticate, login
+from django.contrib.sites.shortcuts import get_current_site
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.db.models import Q
-import json
+from django.shortcuts import redirect
+from django.template.loader import render_to_string
+from django.utils.encoding import force_text
+from jwt.utils import force_bytes
+from django.core.mail import send_mail
+from django.conf import settings
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+
+from registration.tokens import account_activation_token
 
 def signin(req):
     email = req.POST.get('email', False)
@@ -15,6 +26,11 @@ def signin(req):
         jd = json.dumps(resp)
         return HttpResponse(jd, content_type='application/json')
     else:
+        if not userRecord.is_active:
+            resp = {'success': False, 'error': True, 'errorMsg': 'Please verify your account first.'}
+            jd = json.dumps(resp)
+            return HttpResponse(jd, content_type='application/json')
+
         user = authenticate(username=userRecord.username, password=password)
         if (user is None):
             resp = {'success': False, 'error': True, 'errorMsg': 'Invalid login credentials'}
@@ -35,10 +51,18 @@ def signUp(req):
     except User.DoesNotExist:
         newPerson = User(email=email, username=email)
         newPerson.set_password(password)
+        newPerson.is_active = False
         newPerson.save()
-        myUser = authenticate(username=email, password=password)
-        login(req, myUser)
-        response = {'success': True, 'error': False, 'redirect': '/dashboard/'}
+        current_site = get_current_site(req)
+        message = render_to_string('registration/account_activation_email.html', {
+            'user': newPerson,
+            'domain': current_site.domain,
+            'uid': urlsafe_base64_encode(force_bytes(str(newPerson.pk))).decode("utf-8"),
+            'token': account_activation_token.make_token(newPerson),
+        })
+        send_mail('Please confirm your email address', message, settings.EMAIL_HOST_USER, [newPerson.email])
+
+        response = {'success': True, 'error': False, 'redirect': '/account/activate/'}
         jd = json.dumps(response)
         return HttpResponse(jd, content_type='application/json')
     else:
@@ -46,3 +70,15 @@ def signUp(req):
             response = {'success': False, 'error': True, 'errorMsg': 'Email already exists'}
             jd = json.dumps(response)
             return HttpResponse(jd, content_type='application/json')
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return redirect('/')
